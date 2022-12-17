be3c89ecdaae626115c917dcf22e3fbf3f405175
Teisun <103859595+teisun01@users.noreply.github.com>
2022-12-17 01:28:47 -0300

    Add files via upload

---
+++ js/start.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true
+  });
+
+  $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+
+  $(document).ready(function(){
+    $('.slider').slider();
+  });
